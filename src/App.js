163fa66a331fb615377a66b377c8bd3da56d0f94
Yash <yashshukla336@gmail.com>
2023-05-24 12:56:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import "./App.css";
 
-function App() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { id: Date.now(), text: action.payload }];
+    case "DELETE_TODO":
+      return state.filter((todo) => todo.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [inputText, setInputText] = useState("");
+
+  const addTodo = () => {
+    if (inputText.trim() !== "") {
+      dispatch({ type: "ADD_TODO", payload: inputText });
+      setInputText("");
+    }
+  };
+
+  const deleteTodo = (id) => {
+    dispatch({ type: "DELETE_TODO", payload: id });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <h1 className="heading">Todo App</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Enter a todo"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button className="btn-add" onClick={addTodo}>
+          Add Todo
+        </button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button className="btn-dele" onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
